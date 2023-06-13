6f4255f153accb256eb40410e1d6968fb3dafe1c
a0918959733@gmail.com <a0918959733@gmail.com>
2023-06-13 15:35:30 +0800

    登入及登出

---
+++ long_term_care/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace long_term_care.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string id { get; set; }
+
+        public string password { get; set; }
+    }
+   
+
+}
