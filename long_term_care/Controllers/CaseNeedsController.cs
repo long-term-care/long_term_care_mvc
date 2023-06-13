b5fe86dfa181d8e3ec2bfc2487bcf7f297fbaa22
Halston <tsengyiyi1031@gmail.com>
2023-06-13 16:28:33 +0800

    Elder Care

---
+++ long_term_care/Controllers/CaseNeedsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using long_term_care.Models;
+
+namespace long_term_care.Controllers
+{
+    public class CaseNeedsController : Controller
+    {
+        private readonly longtermcareContext _context;
+
+        public CaseNeedsController(longtermcareContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CaseNeeds
+        public async Task<IActionResult> Index()
+        {
+            var longtermcareContext = _context.CaseNeeds.Include(c => c.CaseNoNavigation);
+            return View(await longtermcareContext.ToListAsync());
+        }
+
+        // GET: CaseNeeds/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caseNeed = await _context.CaseNeeds
+                .Include(c => c.CaseNoNavigation)
+                .FirstOrDefaultAsync(m => m.CaseNeedId == id);
+            if (caseNeed == null)
+            {
+                return NotFound();
+            }
+
+            return View(caseNeed);
+        }
+
+        // GET: CaseNeeds/Create
+        public IActionResult Create()
+        {
+            ViewData["CaseNo"] = new SelectList(_context.CaseInfors, "CaseNo", "CaseNo");
+            return View();
+        }
+
+        // POST: CaseNeeds/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CaseNeedId,CaseNo,CaseRead,CaseFami,CaseCons,CaseSpeak,CaseAct,CaseMed,CaseSee,CaseHear,CaseEat,CaseCare,CaseView1,CaseView2,CaseView3,CaseView4,CaseView5,CaseView6,CaseView7,CaseView8,CaseView9")] CaseNeed caseNeed)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(caseNeed);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CaseNo"] = new SelectList(_context.CaseInfors, "CaseNo", "CaseNo", caseNeed.CaseNo);
+            return View(caseNeed);
+        }
+
+        // GET: CaseNeeds/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caseNeed = await _context.CaseNeeds.FindAsync(id);
+            if (caseNeed == null)
+            {
+                return NotFound();
+            }
+            ViewData["CaseNo"] = new SelectList(_context.CaseInfors, "CaseNo", "CaseNo", caseNeed.CaseNo);
+            return View(caseNeed);
+        }
+
+        // POST: CaseNeeds/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CaseNeedId,CaseNo,CaseRead,CaseFami,CaseCons,CaseSpeak,CaseAct,CaseMed,CaseSee,CaseHear,CaseEat,CaseCare,CaseView1,CaseView2,CaseView3,CaseView4,CaseView5,CaseView6,CaseView7,CaseView8,CaseView9")] CaseNeed caseNeed)
+        {
+            if (id != caseNeed.CaseNeedId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(caseNeed);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CaseNeedExists(caseNeed.CaseNeedId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CaseNo"] = new SelectList(_context.CaseInfors, "CaseNo", "CaseNo", caseNeed.CaseNo);
+            return View(caseNeed);
+        }
+
+        // GET: CaseNeeds/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caseNeed = await _context.CaseNeeds
+                .Include(c => c.CaseNoNavigation)
+                .FirstOrDefaultAsync(m => m.CaseNeedId == id);
+            if (caseNeed == null)
+            {
+                return NotFound();
+            }
+
+            return View(caseNeed);
+        }
+
+        // POST: CaseNeeds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var caseNeed = await _context.CaseNeeds.FindAsync(id);
+            _context.CaseNeeds.Remove(caseNeed);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CaseNeedExists(string id)
+        {
+            return _context.CaseNeeds.Any(e => e.CaseNeedId == id);
+        }
+    }
+}
--- long_term_care/wwwroot/js/site.js
+++ long_term_care/wwwroot/js/site.js
@@ -1,4 +1,126 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    closeMessageBox
+    var volunteerMsgBox = $('#volunteerMsgBox');
+    var elderMsgBox = $('#elderMsgBox');
+    var translucentOverlay = '<div class="translucent-overlay"></div>';
+    var messageBox = $('#messageBox');
+    var message_Box = $('.message-box');
+    var generatedOrderNumbers = [];
 
-// Write your JavaScript code.
+    function generateOrderNumber() {
+        var orderNumber;
+        var isDuplicate = true;
+
+        while (isDuplicate) {
+            orderNumber = "A" + (Math.floor(Math.random() * 999999) + 1).toString().padStart(6, '0');
+            isDuplicate = generatedOrderNumbers.includes(orderNumber);
+        }
+
+        generatedOrderNumbers.push(orderNumber);
+        return orderNumber;
+    }
+
+
+    $("#CaseView4Other").change(function () {
+        if ($(this).is(":checked")) {
+            $("input[name='CaseView4']").prop("disabled", false);
+        } else {
+            $("input[name='CaseView4']").prop("disabled", true);
+        }
+    });
+    $("#caseView1No").change(function () {
+        if ($(this).is(":checked")) {
+            $("input[name='CaseView1']").prop("disabled", false);
+        } else {
+            $("input[name='CaseView1']").prop("disabled", true);
+        }
+    });
+
+    $("#CaseView6AO").change(function () {
+        if ($(this).is(":checked")) {
+            $("input[name='CaseView6']").prop("disabled", false);
+        } else {
+            $("input[name='CaseView6']").prop("disabled", true);
+        }
+    });
+
+    $('#orderNumberInput').val(generateOrderNumber());
+
+    $('.question input[type="checkbox"]').change(function () {
+        $(this).siblings('input[type="checkbox"]').prop('checked', false);
+    });
+
+    $('#volunteer, #elder').on('click', function () {
+        var id = $(this).attr('id');
+        var otherId = (id === 'volunteer') ? 'elder' : 'volunteer';
+        $('#register, #identity').addClass('hidden');
+        $('#fullscreen-image').css('background-image', 'url("../Resources/background.png")');
+        volunteerMsgBox.toggleClass('hidden', id !== 'volunteer').toggleClass('large', id === 'volunteer');
+        elderMsgBox.toggleClass('hidden', id !== 'elder').toggleClass('large', id === 'elder');
+        $(this).add('#' + otherId).hide();
+        $('body').append(translucentOverlay);
+    });
+    $('#close, #closeVolunteer, #closeElder').on('click', function () {
+        closeMessageBox();
+    });
+    $('.closeMessageBox').not('#closeVolunteer, #closeElder, #close').on('click', function () {
+        closeMessageBoxBTN();
+    });
+    function closeMessageBox() {
+        volunteerMsgBox.addClass('hidden');
+        elderMsgBox.addClass('hidden');
+        message_Box.fadeOut();
+        messageBox.fadeOut();
+        $('.translucent-overlay').remove();
+    }
+    $('.closeMessageBox').click(function (e) {
+        e.preventDefault();
+        var valid = true;
+        $('input[required]').each(function () {
+            if ($(this).val() === '') {
+                valid = false;
+                return false;
+            }
+        });
+        if ($('option').val() === 'na') {
+            valid = false;
+        }
+        if (valid) {
+            $(this).closest('.container').parent().addClass('hidden');
+        }
+    });
+
+
+    var currentYear = new Date().getFullYear();
+    var startYear = currentYear - 112;
+    var endYear = currentYear + 112;
+    var yearSelect = $('.yearSelect');
+    for (var year = startYear; year <= endYear; year++) {
+        yearSelect.append($('<option>', {
+            value: year,
+            text: year
+        }));
+    }
+    var months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+    var monthSelect = $('.monthSelect');
+    $.each(months, function (index, month) {
+        var monthValue = index + 1;
+        monthSelect.append($('<option>', {
+            value: monthValue,
+            text: month
+        }));
+    });
+    var daysInMonth = 31;
+    var daySelect = $('.daySelect');
+    for (var day = 1; day <= daysInMonth; day++) {
+        daySelect.append($('<option>', {
+            value: day,
+            text: day
+        }));
+    }
+
+    $('#renewPassword').click(function () {
+        $('body').append(translucentOverlay);
+        messageBox.fadeIn().addClass('large');
+    });
+});
