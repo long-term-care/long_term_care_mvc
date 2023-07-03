bb2f54127f0f01a98b502a873d7d061a7e47238c
a0918959733@gmail.com <a0918959733@gmail.com>
2023-07-03 19:27:30 +0800

    課表與活動合併

--- long_term_care/Controllers/LectureClassesController.cs
+++ long_term_care/Controllers/LectureClassesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -64,10 +65,114 @@ namespace long_term_care.Controllers
         {
             var lectureClasses = await _context.LectureClasses.ToListAsync();
 
-            DateTime today = DateTime.Today;
-            var act =  _context.LectureTables.Where(x=>x.LecDate == today);
+            
             
             return View(lectureClasses);
         }
+        public IActionResult Act()
+        {
+            // 获取本周的日期范围
+            DateTime today = DateTime.Today;
+            int diff = (7 + (today.DayOfWeek - DayOfWeek.Monday)) % 7;
+            DateTime startOfWeek = today.AddDays(-1 * diff);
+            DateTime endOfWeek = startOfWeek.AddDays(6);
+
+            // 从数据库中获取活动表和课表数据
+            List<LectureTable> activityList = _context.LectureTables.ToList();
+            List<LectureClass> lectureClasses = _context.LectureClasses.ToList();
+
+            // 遍历活动表的数据，将活动添加到本周的课表中
+            // 遍歷活動表的資料，將活動加入到本週的課表中
+            foreach (var activity in activityList)
+            {
+                if (activity.LecDate >= startOfWeek && activity.LecDate <= endOfWeek)
+                {
+                    // 檢查是否已經存在對應時段的課表項目
+                    var existingLectureClass = lectureClasses.FirstOrDefault(l => l.SchWeek == GetDayOfWeek(activity.LecDate));
+
+                    if (existingLectureClass != null)
+                    {
+                        // 更新對應時段的活動主題
+                        switch (GetTimeSlot(activity.LecDate))
+                        {
+                            case "SchA":
+                                existingLectureClass.SchA = activity.LecTheme;
+                                break;
+                            case "SchB":
+                                existingLectureClass.SchB = activity.LecTheme;
+                                break;
+                            case "SchC":
+                                existingLectureClass.SchC = activity.LecTheme;
+                                break;
+                            case "SchD":
+                                existingLectureClass.SchD = activity.LecTheme;
+                                break;
+                            case "SchE":
+                                existingLectureClass.SchE = activity.LecTheme;
+                                break;
+                        }
+
+                    }
+                }
+            }
+
+            // 将课表数据传递给视图并显示
+            return View(lectureClasses);
+        }
+        public string GetDayOfWeek(DateTime date)
+        {
+            switch (date.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    return "星期一";
+                case DayOfWeek.Tuesday:
+                    return "星期二";
+                case DayOfWeek.Wednesday:
+                    return "星期三";
+                case DayOfWeek.Thursday:
+                    return "星期四";
+                case DayOfWeek.Friday:
+                    return "星期五";
+                case DayOfWeek.Saturday:
+                    return "星期六";
+                case DayOfWeek.Sunday:
+                    return "星期日";
+                default:
+                    return string.Empty;
+            }
+        }
+        public string GetTimeSlot(DateTime date)
+        {
+            // 使用日期（時間）作為判斷依據，設置相應的課表時段
+            if (date.TimeOfDay >= new TimeSpan(7, 30, 0) && date.TimeOfDay < new TimeSpan(8, 30, 0))
+            {
+                return "SchA";
+            }
+            else if (date.TimeOfDay >= new TimeSpan(8, 30, 0) && date.TimeOfDay < new TimeSpan(9, 0, 0))
+            {
+                return "SchB";
+            }
+            else if (date.TimeOfDay >= new TimeSpan(9, 0, 0) && date.TimeOfDay < new TimeSpan(9, 30, 0))
+            {
+                return "SchC";
+            }
+            else if (date.TimeOfDay >= new TimeSpan(9, 30, 0) && date.TimeOfDay < new TimeSpan(12, 0, 0))
+            {
+                return "SchD";
+            }
+            else if (date.TimeOfDay >= new TimeSpan(13, 0, 0) && date.TimeOfDay < new TimeSpan(16, 0, 0))
+            {
+                return "SchE";
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+
+
+
+
     }
 }
